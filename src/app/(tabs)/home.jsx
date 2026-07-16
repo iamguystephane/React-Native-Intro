31e7fe15c339fe4646f5c046bf0f49d379483a46
iamguystephane <forlemustephane@gmail.com>
2026-07-16 14:43:17 +0100

    Revert "modified code from the test branch"
    This reverts commit fa0c9d0214c0de4e8180b0a5fe1981bafd8b9435.

--- src/app/(tabs)/home.jsx
+++ src/app/(tabs)/home.jsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Home() {
   return (
     <SafeAreaView>
-      <Text> Home Back Home </Text>
+      <Text> Home Sweet Home </Text>
     </SafeAreaView>
   );
 }
